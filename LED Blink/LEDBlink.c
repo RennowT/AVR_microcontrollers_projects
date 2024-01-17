643bff13ce83e1858a19b18fc8496da0ce4e4333
Matheus Renó Torres <124406908+RennowT@users.noreply.github.com>
2024-01-17 08:45:46 -0300

    Update LEDBlink.c

--- LED Blink/LEDBlink.c
+++ LED Blink/LEDBlink.c
@@ -1,46 +1,54 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED_PIN PD2 // LED pin (pin 4 on atmega328p)
-#define BUTTON_PIN PB1 // Button pin (pin 9 on atmega328p)
-
-#define HIGH_TIME 500
-#define LOW_TIME 500
+#define LED_PIN PD2     // LED pin (pin 4 on ATmega328p)
+#define BUTTON_PIN PB1  // Button pin (pin 9 on ATmega328p)
 
 // Function to check if the pull-up button is pressed
 _Bool buttonPressed(uint8_t button) {
-  return !(PINB & button); // Returns 1 if PINB & button is 0 (button is pressed)
+  return !(PINB & (1 << button)); // Returns 1 if PINB & button is 0 (button is pressed)
 }
 
 // Function to control LED flashing pattern
 void ledFlashingPattern(uint16_t highTime, uint16_t lowTime) {
-  PORTD |= LED_PIN;      // Turn on the LED
+  PORTD |= (1 << LED_PIN);      // Turn on the LED
   _delay_ms(highTime);
-  PORTD &= ~LED_PIN;     // Turn off the LED
+  PORTD &= ~(1 << LED_PIN);     // Turn off the LED
   _delay_ms(lowTime);
 }
 
 int main(void) {
-  DDRD |= LED_PIN;      // Set the pin as output (0b00000100)
-  DDRB &= ~BUTTON_PIN;  // Set the pin as input (0b00000010)
+  DDRD |= (1 << LED_PIN);      // Set the pin as output (0b00000100)
+  DDRB &= ~(1 << BUTTON_PIN);  // Set the pin as input (0b00000010)
 
-  _Bool buttonPattern = 0;
+  uint8_t buttonPattern = 0;
 
   while (1) {
     // Toggle button pattern when the button is pressed
     if (buttonPressed(BUTTON_PIN)) {
-      buttonPattern = !buttonPattern;
-      
+      buttonPattern++;
       // Wait until the button is released
       while (buttonPressed(BUTTON_PIN))
         _delay_ms(1);
     } 
 
-    // If the button pattern is active, use the flashing pattern; otherwise, turn off the LED
-    if (buttonPattern) {
-      ledFlashingPattern(HIGH_TIME, LOW_TIME); // Pattern when the button is pressed
-    } else {
-      PORTD &= ~LED_PIN; // Turn off the LED
+    // Use a switch statement to handle different button patterns
+    switch (buttonPattern)
+    {
+    case 0:
+      PORTD &= ~(1 << LED_PIN); // Turn off the LED
+      break;
+    case 1: 
+      ledFlashingPattern(500, 500);  // Pattern 1: 500ms on, 500ms off
+      break;
+    case 2: 
+      ledFlashingPattern(800, 200);  // Pattern 2: 800ms on, 200ms off
+      break;
+    case 3:
+      ledFlashingPattern(200, 200);  // Pattern 3: 200ms on, 200ms off
+      break;
+    default:
+      break;
     }
 
     _delay_ms(10);
